f369683892f1986d7d574db1c550a679a961bcf5
Geoff Rowland <rowland.geoff@gmail.com>
2021-05-18 13:39:47 +0100

    Create strode.js

---
+++ strode.js
@@ -0,0 +1,117 @@
+var strodeData ={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "Swimming Pool",
+        "description": "Water sports"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73294,
+          51.12929
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "A Block",
+        "description": "The Learning Zone, Hair, Beauty and Barbering, The Refectory, Student Support Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73303,
+          51.13029
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "B Block",
+        "description": "Reception"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73402,
+          51.13047
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "D Block",
+        "description": "Higher Education and Skills"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73396,
+          51.13019
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "E Block",
+        "description": "The Brasserie, The Hospitality Training Kitchen, Vocational Studies, Science Labs"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73189,
+          51.12922
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "J Block",
+        "description": "A Level Teaching Rooms"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73207,
+          51.12948
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "M Block",
+        "description": "T Level Childcare, Health and Social Care"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73257,
+          51.12965
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "C Block",
+        "description": "Engineering Department, Sculpture and Modelling, Music Studio and Practice Rooms, Media Production Suites, Coomputing Rooms"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.73348,
+          51.12983
+        ]
+      }
+    }
+  ]
+};
